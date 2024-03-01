575979d57f3080a14077fd479e9d68c9393e5a1f
Vasil Dimitrov <v.b.dimitrow@gmail.com>
2024-03-01 21:12:09 +0200

    CountingSheep task added with solution

---
+++ C#/Tasks - Part 1/CountingSheep/Program.cs
@@ -0,0 +1,35 @@
+﻿namespace CountingSheep
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			/*Consider an array/list of sheep where some sheep may be missing from their place. We need a function that counts the number of sheep present in the array (true means present).
+
+			For example:
+			[true,  true,  true,  false,
+			  true,  true,  true,  true ,
+			  true,  false, true,  false,
+			  true,  false, false, true ,
+			  true,  true,  true,  true ,
+			  false, false, true,  true]
+
+			The correct answer would be 17.
+
+			Hint: Don't forget to check for bad values like null */
+			var count = CountSheeps(new bool[] { true,  true,  true,  false,
+			  true,  true,  true,  true ,
+			  true,  false, true,  false,
+			  true,  false, false, true ,
+			  true,  true,  true,  true ,
+			  false, false, true,  true });
+
+			Console.WriteLine(count);
+		}
+
+		public static int CountSheeps(bool[] sheeps)
+		{
+			return sheeps.Count(x => x == true);
+		}
+	}
+}
