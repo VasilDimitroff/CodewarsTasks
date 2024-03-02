c2ac31b339de751b061ed576c4c9ee574ef927e0
Vasil Dimitrov <v.b.dimitrow@gmail.com>
2024-03-02 21:23:37 +0200

    Find The Parity Outlier task added with solution

---
+++ C#/Tasks - Part 1/FindTheParityOutlier/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace FindTheParityOutlier
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			/* You are given an array (which will have a length of at least 3, but could be very large) containing integers.
+			The array is either entirely comprised of odd integers or entirely comprised of even integers 
+			except for a single integer N. Write a method that takes the array as an argument and returns this "outlier"
+			 
+			Examples:
+			[2, 4, 0, 100, 4, 11, 2602, 36] -->  11 (the only odd number)
+			[160, 3, 1719, 19, 11, 13, -21] --> 160 (the only even number)  */
+
+			Console.WriteLine(Find(new int[] { 160, 3, 1719, 19, 11, 13, -21}));
+		}
+
+		public static int Find(int[] integers)
+		{
+			var countOfOdds = integers.Where(x => x % 2 != 0).Count();
+			var countOfEvens = integers.Where(x => x % 2 == 0).Count();
+
+			if (countOfEvens > countOfOdds)
+			{
+				return integers.FirstOrDefault(x => x % 2 != 0);
+			}
+
+			return integers.FirstOrDefault(x => x % 2 == 0);
+		}
+	}
+}
