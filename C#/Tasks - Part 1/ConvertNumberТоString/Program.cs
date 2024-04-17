84c2328968e4605934314e2b70130deefab72bbd
Vasil Dimitrov <v.b.dimitrow@gmail.com>
2024-04-17 19:07:44 +0300

    NumberToString task

---
+++ C#/Tasks - Part 1/ConvertNumberТоString/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace ConvertNumberТоString
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+             * We need a function that can transform a number (integer) into a string.
+               What ways of achieving this do you know?
+
+            123  --> "123"
+            999  --> "999"
+            -100 --> "-100"
+             */
+            ConvertNumberToString(123);
+        }
+
+        static string ConvertNumberToString(int number) => number.ToString();
+    }
+}
